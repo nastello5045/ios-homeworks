72b779d82c432a8c3502052fa29f2f7deded9e3f
nastello5045 <nastello@inbox.ru>
2022-05-16 17:39:38 +0400

    Initial Commit

---
+++ Shared/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Shared
+//
+//  Created by Spiridonova Anastasia on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
